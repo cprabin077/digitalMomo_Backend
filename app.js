5abb30fb350d3071ca258497671449aeeaf266b3
= <prabin.chaudhary3107@gmail.com>
2026-02-10 19:52:53 +0545

    add cart controller

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const productRoute = require("./routes/admin/productRoute")
 const adminUsersRoute = require("./routes/admin/adminUsersRoute")
 const userReviewRoute = require("./routes/user/userReviewRoute")
 const profileRoute = require("./routes/user/profileRoute")
+const cartRoute = require("./routes/user/cartRoute")
 
 
 // Routes end here
@@ -42,6 +43,7 @@ app.use("/api/products",productRoute)
 app.use("/api/admin", adminUsersRoute)
 app.use("/api/reviews",userReviewRoute)
 app.use("/api/profile",profileRoute)
+app.use("/api/cart",cartRoute)
 
 
 
---
+++ controller/user/cart/cartController.js
@@ -0,0 +1,61 @@
+const Product = require("../../../model/productModel")
+const User = require("../../../model/userModel")
+
+exports.addToCart = async (req, res)=>{
+    // userId, productId
+    const userId = req.user.id 
+    const {productId} = req.params
+    if(!productId){
+        return res.status(400).json({
+            message: " Please provide productId"
+        })
+    }
+    const productExist = await Product.findById(productId)
+    if(!productExist){
+        return res.status(404).json({
+            message: "No product with that productId"
+        })
+    }
+    const user = await User.findById(userId)
+    user.cart.push(productId)
+    await user.save()
+    res.status(200).json({
+        message: "Product added to cart"
+    })
+
+}
+
+// GET Cart Items
+exports.getMyCartItems = async(req, res) =>{
+    const userId = req.user.id
+    const userData = await User.findById(userId).populate({
+        path : "cart",
+        select: "-productStatus"
+    })
+    //const cartData = userData.cart
+    res.status(200).json({
+        message: "Cart Item fetched successfully",
+        data: userData.cart
+    })
+}
+
+//Delete Cart Items
+exports.deleteItemFromCart = async(req, res)=>{
+    const {productId} = req.params
+    const userId = req.user.id
+    
+    // check if that product exists or not
+    const product = await Product.findById(productId)
+    if(!product){
+        return res.status(404).json({
+            messsage: "No product with that  productId"
+        })
+    }
+    //get user cart
+    const user = await User.findById(userId)
+    user.cart = user.cart.filter(pId=>pId != productId) //[1,2,3] ==> 2 ==> filter ==> [1,3]
+    await user.save()
+    res.status(200).json({
+        message: "Item removed from cart"
+    })
+}  
